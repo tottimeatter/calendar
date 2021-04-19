2e13d2272415ddebb546131501b053568d5326b9
JordiCarne <carnetorrescasana@gmail.com>
2021-04-19 17:27:18 +0200

    Remove firebase-config from the repo

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyASvMuQUH8yx0TtH_1qI5iHwJawdYE5O9k",
-    authDomain: "react-calendar-c0b67.firebaseapp.com",
-    projectId: "react-calendar-c0b67",
-    databaseURL: "https://react-calendar-c0b67-default-rtdb.firebaseio.com",
-    storageBucket: "react-calendar-c0b67.appspot.com",
-    messagingSenderId: "210587085048",
-    appId: "1:210587085048:web:e74f891568c71a7c62b30d"
-  };
